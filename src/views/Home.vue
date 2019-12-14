17c6f770c3da4f28d451195e22dca8896332a56a
bartstachurski <bart.stachurski@gmail.com>
2019-12-14 16:51:57 -0500

    make created function to pull user data from backend for now harcoding user 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h1>{{ user }}</h1>
   </div>
 </template>
 
@@ -8,13 +9,20 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      user: {}
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("/api/users/1").then(response => {
+      this.user = response.data;
+    });
+  },
   methods: {}
 };
 </script>
